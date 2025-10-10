65f38470c616fe4b775c05d2f6902238abe3cd5d
AndS56 <andree567890@gmail.com>
2025-10-09 20:20:56 -0500

    Add files via upload

---
+++ BusTrack-Frontend/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MainToolbar from './shared/presentation/components/MainToolbar.vue';
+
+defineOptions({
+  name: 'App'
+});
+</script>
+
+<template>
+  <div id="app">
+    <MainToolbar />
+
+  </div>
+</template>
+
+<style scoped>
+/* Estilos globales para app */
+body {
+  margin: 0;
+  font-family: sans-serif; /* Fuente base para el resto de la app */
+}
+</style>
