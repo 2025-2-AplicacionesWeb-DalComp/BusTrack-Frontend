da94633b4c00c55cc00c0c6c8c18e7b5f13da7d3
AndS56 <andree567890@gmail.com>
2025-10-10 00:37:53 -0500

    Update App.vue

--- BusTrack-Frontend/src/App.vue
+++ BusTrack-Frontend/src/App.vue
@@ -9,14 +9,20 @@ defineOptions({
 <template>
   <div id="app">
     <MainToolbar />
-
+    <router-view />
   </div>
 </template>
 
-<style scoped>
+<style>
 /* Estilos globales para app */
 body {
   margin: 0;
-  font-family: sans-serif; /* Fuente base para el resto de la app */
+  font-family: sans-serif;
+  background-color: white;
+}
+
+#app {
+  min-height: 100vh;
+  background-color: white;
 }
 </style>
